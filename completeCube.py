8b0c8ec08660dbe0db755c39c76f190a8cef2d20
SHA65536 <Ajrtcuev@gmail.com>
2020-03-14 21:21:22 +0200

    [RubiksCube] Moved completed cube to seperate file, added hash function, made concept test

---
+++ completeCube.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+complete = np.array([
+            [
+                #Left Slice
+                [
+                    #Bottom Side
+                    ["O","X","B","X","W","X"],["X","X","B","X","W","X"],["X","R","B","X","W","X"]
+                ],
+                [
+                    #Middle Side
+                    ["O","X","B","X","X","X"],["X","X","B","X","X","X"],["X","R","B","X","X","X"]
+                ],
+                [
+                    #Top Side
+                    ["O","X","B","X","W","Y"],["X","X","B","X","X","Y"],["X","R","B","X","X","Y"]
+                ]
+            ],
+            [
+                #Middle Slice
+                [
+                    #Bottom Side
+                    ["O","X","X","X","W","X"],["X","X","X","X","W","X"],["X","R","X","X","W","X"]
+                ],
+                [
+                    #Middle Side
+                    ["O","X","X","X","X","X"],["X","X","X","X","X","X"],["X","R","X","X","X","X"]
+                ],
+                [
+                    #Top Side
+                    ["O","X","X","X","X","Y"],["X","X","X","X","X","Y"],["X","R","X","X","X","Y"]
+                ]
+            ],
+            [
+                #Right Slice
+                [
+                    #Bottom Side
+                    ["O","X","X","G","W","X"],["X","X","X","G","W","X"],["X","R","X","G","W","X"]
+                ],
+                [
+                    #Middle Side
+                    ["O","X","X","G","X","X"],["X","X","X","G","X","X"],["X","R","X","G","X","X"]
+                ],
+                [
+                    #Top Side
+                    ["O","X","X","G","X","Y"],["X","X","X","G","X","Y"],["X","R","X","G","X","Y"]
+                ]
+            ]
+        ])
+
+completeHash = "OXBXWXXXBXWXXRBXWXOXBXXXXXBXXXXRBXXXOXBXWYXXBXXYXRBXXYOXXXWXXXXXWXXRXXWXOXXXXXXXXXXXXRXXXXOXXXXYXXXXXYXRXXXYOXXGWXXXXGWXXRXGWXOXXGXXXXXGXXXRXGXXOXXGXYXXXGXYXRXGXY"
