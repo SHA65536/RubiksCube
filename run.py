06616751d8cf54c0818102a130ec90a303fda924
SHA65536 <53488697+SHA65536@users.noreply.github.com>
2020-03-14 21:21:22 +0200

    [RubiksCube] Moved completed cube to seperate file, added hash function, made concept test

--- run.py
+++ run.py
@@ -1,51 +1,27 @@
 import numpy as np
 import draw
+import moves
+from completeCube import complete, completeHash
+
+def hashCube(cube):
+    hashString = ""
+    for i in range(3):
+        for j in range(3):
+            for k in range(3):
+                for l in range(6):
+                    hashString += cube[i][j][k][l]
+    return hashString
 
 if __name__ == "__main__":
-    testCube = np.array([
-            [
-                #Left Slice
-                [
-                    #Bottom Side
-                    ["O","X","B","X","W","X"],["X","X","B","X","W","X"],["X","R","B","X","W","X"]
-                ],
-                [
-                    #Middle Side
-                    ["O","X","B","X","X","X"],["X","X","B","X","X","X"],["X","R","B","X","X","X"]
-                ],
-                [
-                    #Top Side
-                    ["O","X","B","X","W","Y"],["X","X","B","X","X","Y"],["X","R","B","X","X","Y"]
-                ]
-            ],
-            [
-                #Middle Slice
-                [
-                    #Bottom Side
-                    ["O","X","X","X","W","X"],["X","X","X","X","W","X"],["X","R","X","X","W","X"]
-                ],
-                [
-                    #Middle Side
-                    ["O","X","X","X","X","X"],["X","X","X","X","X","X"],["X","R","X","X","X","X"]
-                ],
-                [
-                    #Top Side
-                    ["O","X","X","X","X","Y"],["X","X","X","X","X","Y"],["X","R","X","X","X","Y"]
-                ]
-            ],
-            [
-                #Right Slice
-                [
-                    #Bottom Side
-                    ["O","X","X","G","W","X"],["X","X","X","G","W","X"],["X","R","X","G","W","X"]
-                ],
-                [
-                    #Middle Side
-                    ["O","X","X","G","X","X"],["X","X","X","G","X","X"],["X","R","X","G","X","X"]
-                ],
-                [
-                    #Top Side
-                    ["O","X","X","G","X","Y"],["X","X","X","G","X","Y"],["X","R","X","G","X","Y"]
-                ]
-            ]
-        ])
+    cube = complete
+    currHash = ""
+    cnt = 0
+    while currHash != completeHash:
+        draw.createImage(cube, f"output/{cnt:03}.png")
+        cube = moves.upForward(cube)
+        cnt+=1
+        draw.createImage(cube, f"output/{cnt:03}.png")
+        cube = moves.rightForward(cube)
+        cnt+=1
+        currHash = hashCube(cube)
+    draw.createImage(cube, f"output/{cnt:03}.png")
