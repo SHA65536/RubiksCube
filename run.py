f7ec2782efa6ee01d344a4031792b7b85f08989a
SHA65536 <Ajrtcuev@gmail.com>
2020-03-15 18:35:24 +0200

    [RubiksCube] Added gid making functionality, and analitics for the cycle calculation

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 import numpy as np
 import draw
 import moves
+from os import remove
 from completeCube import complete, completeHash
 from itertools import combinations_with_replacement
+import imageio
+from pygifsicle import optimize
 
 possibleMoves = "rRlLuUdDfFbB"
 
@@ -27,6 +30,46 @@ def makeInstructions(start=1):
             yield ''.join(x)
         start+=1
 
+def makeImagesFromString(moveString):
+    cycleCount = 0
+    moveCount = 0
+    filenames = []
+    cube = complete
+    currHash = ""
+    draw.createImage(cube,f"output/{moveString}_{moveCount:06}.png")
+    filenames.append(f"output/{moveString}_{moveCount:06}.png")
+    while currHash != completeHash:
+        cycleCount+=1
+        for move in moveString:
+            moveCount+=1
+            cube = moves.moves[move](cube)
+            draw.createImage(cube,f"output/{moveString}_{moveCount:06}.png")
+            filenames.append(f"output/{moveString}_{moveCount:06}.png")
+        currHash = hashCube(cube)
+    return filenames
+    
+def makeMoviesFromString(moveString):
+    filenames = makeImagesFromString(moveString)
+    with imageio.get_writer(f'output/{moveString}.gif', mode='I') as writer:
+        for filename in filenames:
+            image = imageio.imread(filename)
+            writer.append_data(image)
+    optimize(f'output/{moveString}.gif')
+    for filename in filenames:
+        remove(filename)
+    return f'output/{moveString}.gif'
+
+def highestCycleNumber():
+    highest = ("",0)
+    count = 0
+    with open("output.log",'r') as f:
+        for line in f:
+            instruct, cycles = line.split("\t")
+            count += 1
+            cycles = int(cycles)
+            if cycles > highest[1]:
+                highest = (instruct, cycles)
+    return highest, count
 
 if __name__ == "__main__":
     with open("output.log",'w') as f:
