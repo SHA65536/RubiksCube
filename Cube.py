78b7f7a7c0611c2cfa49f63b4c063bb885d3bed2
SHA65536 <Ajrtcuev@gmail.com>
2020-04-04 01:15:01 +0300

    [RubiksCube] Started dismantling the moves module

--- Cube.py
+++ Cube.py
@@ -1,6 +1,27 @@
 import numpy as np
 
 class Cube():
+    LEFT_SLICE = 0
+    MIDDLE_SLICE = 1
+    RIGHT_SLICE = 2
+
+    BOTTOM_SLICE = 0
+    TOP_SLICE = 2
+
+    BACK_SLICE = 0
+    FRONT_SLICE = 2
+
+    BACK_FACE = 0
+    FRONT_FACE = 1
+    LEFT_FACE = 2
+    RIGHT_FACE = 3
+    BOTTOM_FACE = 4
+    TOP_FACE = 5
+
+    X_AXIS_FACE_TRANSLATION = [5,4,2,3,0,1]
+    Z_AXIS_FACE_TRANSLATION = [0,1,4,5,3,2]
+    Y_AXIS_FACE_TRANSLATION = [3,2,0,1,4,5]
+
     def __init__(self):
         self.data = np.load('BaseCube.npy')
         self.complete_hash = [x for x in np.nditer(self.data)]
@@ -8,3 +29,5 @@ class Cube():
     @property
     def hash(self):
         return [x for x in np.nditer(self.data)]
+
+    
--- moves.py
+++ moves.py
@@ -1,26 +1,7 @@
 import numpy as np
 import draw
 
-LEFT_SLICE = 0
-MIDDLE_SLICE = 1
-RIGHT_SLICE = 2
 
-BOTTOM_SLICE = 0
-TOP_SLICE = 2
-
-BACK_SLICE = 0
-FRONT_SLICE = 2
-
-BACK_FACE = 0
-FRONT_FACE = 1
-LEFT_FACE = 2
-RIGHT_FACE = 3
-BOTTOM_FACE = 4
-TOP_FACE = 5
-
-X_AXIS_FACE_TRANSLATION = [5,4,2,3,0,1]
-Z_AXIS_FACE_TRANSLATION = [0,1,4,5,3,2]
-Y_AXIS_FACE_TRANSLATION = [3,2,0,1,4,5]
 
 def right_forward(source):
     return x_axis(source, True, False)
